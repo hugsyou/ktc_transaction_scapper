cb0d151b07236f81150efd931cbf29781f034813
opart.y <opartyongnoo@gmail.com>
2021-07-11 22:32:29 +0700

    Add Comment

--- index.js
+++ index.js
@@ -6,20 +6,26 @@ const KTC_Password = ''; // <== Input KTC Password
     const puppeteer = require('puppeteer');
 
     const {
+        writeJSONFile,
+    } = require('./Misc');
+
+    const {
         getKTCTransaction,
     } = require('./KTCTransaction');
 
     const browser = await puppeteer.launch({
-        headless: true,
+        headless: true, // Dont show Browser when run this app --- { headless: true }
         args: [
-            '--incognito',
+            '--incognito', // Open Browser with [incognito] Option --- { args: ['--incognito'] }
         ]
     });
 
-    await getKTCTransaction(browser, { KTC_Useranme, KTC_Password }, { convertPrice: true })
-        .then(result1 => { console.log(result1); return result1; })
-        .then(async (result2) => {
-            const writeJSONFile = require('./Misc').writeJSONFile;
+    await getKTCTransaction(browser, { KTC_Useranme, KTC_Password }, { convertPrice: true }) // Scraping KTC Transaction and ConvertOutput [Price] to Object Number
+        .then(result1 => {
+            console.log(result1);
+            return result1;
+        })
+        .then(async (result2) => { // Write Output to JSON File
             await writeJSONFile(result2, 'Output.json');
         })
         .catch(error => console.error(error));
